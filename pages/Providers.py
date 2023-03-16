57db48b9f52b0a13bbfe6400e9ce4c7c0a5f2504
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-17 00:54:03 +0100

    sad

--- pages/Providers.py
+++ pages/Providers.py
@@ -33,7 +33,7 @@ with tab2:
 # Create bar chart sorted by value
   labels = [key for key in providers['Via']]
   keys = [key for key in providers['Percentage']]
-  colors = ['#D2042D', '#702963', '#C04000','#8F8585','#bdbdbd'] 
+  colors = ['#D2042D', '#05D832', '#C04000','#8F8585','#bdbdbd'] 
   fig, ax = plt.subplots()
   
   ax.pie(keys, labels=labels, 
