549d734a23ce67fb300e3c0490449ab60805f3d7
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-16 17:01:59 +0100

    asdasd

--- analsysis_app.py
+++ analsysis_app.py
@@ -24,37 +24,6 @@ with st.sidebar:
   else:
     st.write(' ')
 
-df =pd.read_csv('keywords.csv')
-df = df.sort_values('amount',ascending=False)
-
-# Tabs section
-st.write('Choose format of the data')
-tab1, tab2 , tab3= st.tabs(["Quantity", "Percentage", "Table"])
-
-with tab1:
-
-  st.subheader('Top 15 keywords by amount 📊')
-  fig = plt.figure()
-  plt.bar(df['keywords'], df['amount'])
-  plt.xticks(rotation=90)
-  plt.xlabel('Keyword')
-  plt.ylabel('Amount')
-
-  # Display chart in Streamlit app
-  st.pyplot(fig)
-  
-
-with tab2:
-# Create bar chart sorted by value
-  st.subheader('Top 15 keywords by percentage 📊')
-  fig = plt.figure()
-  plt.bar(df['keywords'], df['amount'])
-  plt.xticks(rotation=90)
-  plt.xlabel('Keyword')
-  plt.ylabel('Amount')
-
-  # Display chart in Streamlit app
-  st.pyplot(fig)
 
 
 
---
+++ pages/Top_keywords.py
@@ -0,0 +1,35 @@
+# Uploading dataframe
+df =pd.read_csv('keywordsfull.csv')
+df = df.sort_values('amount',ascending=False)
+
+# Tabs section
+st.write('Choose format of the data')
+tab1, tab2 , tab3= st.tabs(["Quantity", "Percentage", "Table"])
+
+with tab1:
+
+  st.subheader('Top 15 keywords by amount 📊')
+  fig = plt.figure()
+  plt.bar(df['keywords'], df['amount'])
+  plt.xticks(rotation=90)
+  plt.xlabel('Keyword')
+  plt.ylabel('Amount')
+
+  # Display chart in Streamlit app
+  st.pyplot(fig)
+  
+
+with tab2:
+# Create bar chart sorted by value
+  st.subheader('Top 15 keywords by percentage 📊')
+  fig = plt.figure()
+  plt.bar(df['keywords'], df['amount'])
+  plt.xticks(rotation=90)
+  plt.xlabel('Keyword')
+  plt.ylabel('Amount')
+
+  # Display chart in Streamlit app
+  st.pyplot(fig)
+
+with tab3:
+  st.write(df)
