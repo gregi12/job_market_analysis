e9b313f2b7be1eeb68297d873ccf280819aa0d84
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-16 12:44:12 +0100

    dasd

--- analsysis_app.py
+++ analsysis_app.py
@@ -27,7 +27,7 @@ df =pd.read_csv('keywords.csv')
 df = df.sort_values('amount',ascending=False)
 
 # Create bar chart sorted by value
-
+fig = plt.figure()
 plt.bar(df['keywords'], df['amount'])
 plt.xticks(rotation=90)
 plt.title('Sorted Bar Chart')
