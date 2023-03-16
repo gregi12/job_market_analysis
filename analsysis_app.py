83d3e60dd4dd0eb5799f6ff8ad8b2d8ee043f654
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-16 18:45:40 +0100

    dsa

--- analsysis_app.py
+++ analsysis_app.py
@@ -28,7 +28,6 @@ with st.sidebar:
 st.subheader('Follow instruction below to continue!')
 greet = st.text_input('Write your name, please!')
 if greet:
-  st.balloons()
   st.write('👋 Hey! ',greet," it's nice to meet you" )
   original_title = '<p style="font-family:Courier; font-size: 20px; font-weight:600;">My name is Grzegorz. As we already know each other and you are still here I will give brief overview of this analysis, app and maybe few words about me.</p>'
   st.markdown(original_title, unsafe_allow_html=True)
