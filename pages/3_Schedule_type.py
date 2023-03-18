5311dc49c3cdd00e600d111995e5911f1c66944f
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-18 14:23:00 +0100

    dsad

--- pages/3_Schedule_type.py
+++ pages/3_Schedule_type.py
@@ -7,7 +7,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Utwórz ścieżkę do pliku csv używając ścieżki bazowej i folderu files
-csv_path = os.path.join(BASE_DIR, '..', 'files', 'keywordsfull.csv')
+csv_path = os.path.join(BASE_DIR, '..', 'files', 'Typefull.csv')
 
 # Wczytaj plik csv do dataframe
 df = pd.read_csv(csv_path)
@@ -15,5 +15,16 @@ df = pd.read_csv(csv_path)
 
 
 # Tabs section
+st.subheader("Schedule type ")
 st.write('Choose format of the data')
 tab1, tab2 , tab3= st.tabs(["Quantity", "Percentage", "Table"])
+
+with tab1:
+    st.subheader('Top types of contract📊')
+    fig = plt.figure()
+    colors = ['#D2042D', '#702963', '#C04000','#8F8585','#bdbdbd'] 
+    plt.bar(providers['Via'], providers['Amount'],color=colors,edgecolor='black',linewidth=1)
+    plt.xticks(rotation=90)
+    plt.ylabel('Amount')
+    # Display chart in Streamlit app
+    st.pyplot(fig)
