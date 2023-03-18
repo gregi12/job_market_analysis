f499ae026b05058d115cecccd59c596797e1f07d
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-18 12:26:24 +0100

    dasd

--- analsysis_app.py
+++ analsysis_app.py
@@ -31,33 +31,34 @@ st.subheader('But firstly, follow instruction below to continue!')
 greet = st.text_input('Write your name, nickname or whatever!',st.session_state['greet'])
 submit = st.button("Let's go")
 if submit:
-  st.write('👋 Hey ',greet,", nice to meet you!" )
-  original_title = '<p style="font-family:Courier; font-size: 20px; font-weight:600;">My name is Grzegorz. As we already know each other and you are still here, I will give brief overview of this analysis, app and maybe few words about me.</p>'
-  st.markdown(original_title, unsafe_allow_html=True)
-  st.write(' ')
+  while greet:
+    st.write('👋 Hey ',greet,", nice to meet you!" )
+    original_title = '<p style="font-family:Courier; font-size: 20px; font-weight:600;">My name is Grzegorz. As we already know each other and you are still here, I will give brief overview of this analysis, app and maybe few words about me.</p>'
+    st.markdown(original_title, unsafe_allow_html=True)
+    st.write(' ')
 
-  st.write('If someone is wondering...')
-  with st.expander("Here I explain how I obtained data"):
-    st.write("https://github.com/gregi12/Jobs-offers-project/blob/master/getting_data.py")
-    # Tworzenie trzech przycisków w trzech kolumnach
-  col1, col2, col3 = st.beta_columns(3)
+    st.write('If someone is wondering...')
+    with st.expander("Here I explain how I obtained data"):
+      st.write("https://github.com/gregi12/Jobs-offers-project/blob/master/getting_data.py")
+      # Tworzenie trzech przycisków w trzech kolumnach
+    col1, col2, col3 = st.beta_columns(3)
 
-  # Dodawanie przycisków do kolumn
-  with col1:
-      if st.button("About analysis"):
-        original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;">Sooo, these results comes from over 1200 job offers. Above is link to github file where I give more detailed explanation about how I obtained the data. I extracted valuable data using regular expressions(regex)</p>'
-        st.markdown(original_title, unsafe_allow_html=True)
-        with st.expander("More info here!"):
-          st.write("https://github.com/gregi12/Jobs-offers-project/blob/master/final_analysis.py")
-  with col2:
-      if st.button("About app"):
-        original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;">In upper left corner you can see navigation links, every one of them is for specific part of visualization . Whenever there is info about percentage , it means percentage of all offers after duplicates were deleted, so eventually on 994 offers. I would suggest to use light theme, everything is more clear then. Theme can be changed in settings in upper right corner.</p>'
-        st.markdown(original_title, unsafe_allow_html=True)
-        
+    # Dodawanie przycisków do kolumn
+    with col1:
+        if st.button("About analysis"):
+          original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;">Sooo, these results comes from over 1200 job offers. Above is link to github file where I give more detailed explanation about how I obtained the data. I extracted valuable data using regular expressions(regex)</p>'
+          st.markdown(original_title, unsafe_allow_html=True)
+          with st.expander("More info here!"):
+            st.write("https://github.com/gregi12/Jobs-offers-project/blob/master/final_analysis.py")
+    with col2:
+        if st.button("About app"):
+          original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;">In upper left corner you can see navigation links, every one of them is for specific part of visualization . Whenever there is info about percentage , it means percentage of all offers after duplicates were deleted, so eventually on 994 offers. I would suggest to use light theme, everything is more clear then. Theme can be changed in settings in upper right corner.</p>'
+          st.markdown(original_title, unsafe_allow_html=True)
+          
 
-  with col3:
-      if st.button("About me"):
-        original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;"></p>'
-        st.markdown(original_title, unsafe_allow_html=True)
+    with col3:
+        if st.button("About me"):
+          original_title = '<p style="font-family:Calibri; font-size: 20px; font-weight:600;"></p>'
+          st.markdown(original_title, unsafe_allow_html=True)
 
 
