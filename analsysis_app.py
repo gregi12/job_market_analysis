5f3537a6ffbf8b573a27b24c21e7ef67f5f51cd0
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-18 13:00:14 +0100

    dsada

--- analsysis_app.py
+++ analsysis_app.py
@@ -34,7 +34,7 @@ try:
       st.session_state["greet"] = ""
     else:
       st.session_state["greet"] = greet
-  while st.session_state["greet"]:
+  if st.session_state["greet"]:
     st.write('👋 Hey ',greet,", nice to meet you!" )
     original_title = '<p style="font-family:Courier; font-size: 20px; font-weight:600;">My name is Grzegorz. As we already know each other and you are still here, I will give brief overview of this analysis, app and maybe few words about me.</p>'
     st.markdown(original_title, unsafe_allow_html=True)
