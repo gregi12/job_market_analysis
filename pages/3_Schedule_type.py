c74bd7ff50a3e73554e04d5a285b74d01042644c
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-18 14:59:06 +0100

    das

--- pages/3_Schedule_type.py
+++ pages/3_Schedule_type.py
@@ -28,7 +28,7 @@ with tab2:
     labels = [key for key in df['Type']]
     keys = [key for key in df['Percentage']]
     colors = ['#FC1A00', '#05D832', '#F1FC00','#8F8585'] 
-    explode = (0, 0.1, 0, 0)
+    explode = (0.1, 0.1, 0.1, 0.1)
     fig, ax = plt.subplots()
     
     ax.pie(keys, labels=labels, 
