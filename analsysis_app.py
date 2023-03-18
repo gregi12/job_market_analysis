6d49418163a7a29ebf38b0b3bec1630c12cefa78
gregi12 <114067438+gregi12@users.noreply.github.com>
2023-03-18 12:51:59 +0100

    dasda

--- analsysis_app.py
+++ analsysis_app.py
@@ -32,10 +32,10 @@ greet = st.text_input('Write your name, nickname or whatever!',st.session_state[
 submit = st.button("Let's go")
 if submit:
   if greet == "":
-    st.session_state["greet"] = "Stranger"
+    st.session_state["greet"] = ""
   else:
     st.session_state["greet"] = greet
-if greet or st.session_state["greet"] == "Stranger":
+if greet or st.session_state["greet"] == "":
   st.write('👋 Hey ',greet,", nice to meet you!" )
   original_title = '<p style="font-family:Courier; font-size: 20px; font-weight:600;">My name is Grzegorz. As we already know each other and you are still here, I will give brief overview of this analysis, app and maybe few words about me.</p>'
   st.markdown(original_title, unsafe_allow_html=True)
